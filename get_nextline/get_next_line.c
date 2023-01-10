94b9d1745be3427c64b4a7bf3110851b60aa0552
MustafaMercan <74800307+MustafaMercan@users.noreply.github.com>
2023-01-10 18:13:37 +0300

    Add files via upload

--- get_nextline/get_next_line.c
+++ get_nextline/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mmercan <mmercan@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 14:39:30 by mmercan           #+#    #+#             */
-/*   Updated: 2023/01/10 17:49:00 by mmercan          ###   ########.fr       */
+/*   Updated: 2023/01/10 18:12:47 by mmercan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,14 +44,42 @@ char *get_line(char* str)
 {
     int index;
     char *tmp;
-
+    int i;
+    
+    i = 0;
     index = find_new_line(str);
+    tmp = (char*)malloc(sizeof(char) * ft_strlen(str) + 2);
     if(index == -1)
         return str;
-    tmp = ft_join2(tmp,str,index);
-    printf("tmp -> (%s)\n",tmp);
+        
+    while(str[i] && i < index)
+    {
+        tmp[i] = str[i];
+        i++;
+    }
     return tmp;
     
+}
+char *get_next(char *str)
+{
+    int i;
+    int j = 0;
+    i = 0;
+    
+    while(str[i] && str[i] != '\0')
+    {
+        i++;
+    }
+    int index;
+
+    index = ft_strlen(str) - i ;
+    char *tmp = (char*)malloc(sizeof(char) * index + 1);
+    while(str[i + j] && str[i + j] != '\0')
+    {
+        tmp[j] = str[i + j];
+        j++;
+    }
+    return tmp;
 }
 char* get_next_line(int fd)
 {
@@ -69,11 +97,13 @@ char* get_next_line(int fd)
     */
     
     line = get_data(fd);
-    //line = ft_join(line,"mustafa");
     str = get_line(line);
+    line = get_next(line);
+    //line = ft_join(line,"mustafa");
     
-    index = find_new_line(line);
-    line = ft_strdup(line + index + 1);
+    
+    //index = find_new_line(line);
+    //line = ft_strdup(line + index + 1);
 
     
     
